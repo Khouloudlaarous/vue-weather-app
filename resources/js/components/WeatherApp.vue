f134477bc353d32efe8fdacee02807ce83fe4db8
Khouloudlaarous <khouloudaissa@gmail.com>
2021-05-10 22:42:19 +0100

    untracked files

---
+++ resources/js/components/WeatherApp.vue
@@ -0,0 +1,13 @@
+<template>
+   <div>
+     Example here
+   </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weather app</title>
+
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <script src="/js/app.js" defer></script>
+</head>
+<body class="bg-red-500 h-5">
+khouloud
+<div id="app" class="flex justify-center pt-16">
+    <weather-app></weather-app>
+</div>
+
+</body>
+</html>
